342e6e36d0ddd6aaf42fd945e31c25f4a7b270d6
priyarajesh7696 <rameshpriyait@gmail.com>
2023-11-27 17:10:40 +0530

    Task completed

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Hi,this is callback hell function Task
---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.getElementById("display").innerHTML="10";
+    setTimeout(()=>{
+        document.getElementById("display").innerHTML="9";
+        setTimeout(()=>{
+            document.getElementById("display").innerHTML="8"
+            setTimeout(()=>{
+                document.getElementById("display").innerHTML="7"
+                setTimeout(()=>{
+                    document.getElementById("display").innerHTML="6"
+                    setTimeout(()=>{
+                        document.getElementById("display").innerHTML="5"
+                        setTimeout(()=>{
+                            document.getElementById("display").innerHTML="4"
+                            setTimeout(()=>{
+                                document.getElementById("display").innerHTML="3"
+                                setTimeout(()=>{
+                                    document.getElementById("display").innerHTML="2"
+                                    setTimeout(()=>{
+                                        document.getElementById("display").innerHTML="1"
+                                        setTimeout(()=>{
+                                            document.getElementById("display").innerHTML="🎉🎉Happy Independence Day🎉🎉"
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
